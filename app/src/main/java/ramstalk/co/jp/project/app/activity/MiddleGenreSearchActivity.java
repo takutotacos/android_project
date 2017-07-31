10856a0791ed940632052ae275ebed1766f68555
takuto.sugita <takuto.sugita@jmty.jp>
2017-08-01 08:46:40 +0900

    中ジャンルの取得・表示処理を追加した。

--- app/src/main/java/ramstalk/co/jp/project/app/activity/MiddleGenreSearchActivity.java
+++ app/src/main/java/ramstalk/co/jp/project/app/activity/MiddleGenreSearchActivity.java
@@ -2,15 +2,20 @@ package ramstalk.co.jp.project.app.activity;
 
 import android.content.Context;
 import android.content.Intent;
+import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.AdapterView;
 
 import java.util.List;
 
 import ramstalk.co.jp.project.R;
+import ramstalk.co.jp.project.app.adapter.GenreAdapter;
 import ramstalk.co.jp.project.app.contract.MiddleGenreSearchActivityContract;
 import ramstalk.co.jp.project.app.presenter.MiddleGenreSearchActivityPresenter;
 import ramstalk.co.jp.project.data.MiddleGenre;
+import ramstalk.co.jp.project.databinding.ActivityMiddleGenreSearchBinding;
 import ramstalk.co.jp.project.domain.repository.factory.ApiErrorView;
 
 public class MiddleGenreSearchActivity extends AppCompatActivity implements MiddleGenreSearchActivityContract.View, ApiErrorView {
@@ -18,6 +23,7 @@ public class MiddleGenreSearchActivity extends AppCompatActivity implements Midd
     public static final String MIDDLE_GENRE_ID = "middle_genre_id";
     private String largeGenreId;
     private MiddleGenreSearchActivityContract.UserAction presenter;
+    private ActivityMiddleGenreSearchBinding binding;
 
     public static Intent createIntent(Context context, String middleGenreId) {
         Intent i = new Intent(context, MiddleGenreSearchActivity.class);
@@ -28,7 +34,7 @@ public class MiddleGenreSearchActivity extends AppCompatActivity implements Midd
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_middle_genre_search);
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_middle_genre_search);
 
         largeGenreId = getIntent().getStringExtra(MIDDLE_GENRE_ID);
 
@@ -46,12 +52,25 @@ public class MiddleGenreSearchActivity extends AppCompatActivity implements Midd
 
     @Override
     public void showMiddleGenreList(List<MiddleGenre> middleGenres) {
+        final GenreAdapter<MiddleGenre> largeGenreAdapter = new GenreAdapter<>(getApplicationContext());
+        largeGenreAdapter.setGenreList(middleGenres);
+        binding.lvMiddleGenre.setAdapter(largeGenreAdapter);
+
+        binding.lvMiddleGenre.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String clickedId = String.valueOf(largeGenreAdapter.getItemId(position));
+                Intent i = MiddleGenreSearchActivity.createIntent(getApplicationContext(), clickedId);
+                startActivity(i);
+            }
+        });
 
     }
 
     @Override
     public void hideMiddleGenreList() {
-
+        binding.lvMiddleGenre.setVisibility(View.GONE);
+        binding.tvMiddleGenreNotFound.setVisibility(View.VISIBLE);
     }
 
     @Override
