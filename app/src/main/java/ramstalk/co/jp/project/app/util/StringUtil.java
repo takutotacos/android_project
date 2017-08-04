bb11cd44000da091a6b01e900d2b2a2a39c83531
takuto.sugita <takuto.sugita@jmty.jp>
2017-08-05 07:49:50 +0900

    単体テストでAndroidの標準メソッド`TextUtil.isEmpty`が使えないため。

---
+++ app/src/main/java/ramstalk/co/jp/project/app/util/StringUtil.java
@@ -0,0 +1,13 @@
+package ramstalk.co.jp.project.app.util;
+
+/**
+ * Created by takuto.sugita on 2017/08/05.
+ */
+
+public class StringUtil {
+
+    public static boolean isEmpty(String str) {
+        return str == null || str.isEmpty();
+    }
+
+}
