cc71746d4332c3e94c5d8003c0468d726a3a6671
takuto.sugita <takuto.sugita@jmty.jp>
2017-08-05 07:53:22 +0900

    ユーザ登録を実装

--- app/src/main/java/ramstalk/co/jp/project/app/presenter/SignUpActivityPresenter.java
+++ app/src/main/java/ramstalk/co/jp/project/app/presenter/SignUpActivityPresenter.java
@@ -1,10 +1,11 @@
 package ramstalk.co.jp.project.app.presenter;
 
-import android.text.TextUtils;
-
 import ramstalk.co.jp.project.app.contract.SignUpActivityContract;
+import ramstalk.co.jp.project.app.util.StringUtil;
 import ramstalk.co.jp.project.data.User;
 import ramstalk.co.jp.project.domain.repository.factory.ApiErrorView;
+import ramstalk.co.jp.project.domain.repository.factory.ApiObserver;
+import ramstalk.co.jp.project.domain.repository.factory.ApiUtil;
 
 /**
  * Created by takuto.sugita on 2017/08/01.
@@ -27,20 +28,42 @@ public class SignUpActivityPresenter implements SignUpActivityContract.Presenter
         String password = user.getPassword();
         String passwordConfirmation = user.getPassowrdConfirmation();
 
-        if (TextUtils.isEmpty(email)) {
-            view.showEmailValidationError("からです。");
+        if (!validateUserInput(name, email, password, passwordConfirmation)) {
+            return;
         }
-        if (TextUtils.isEmpty(password)) {
+
+        ApiUtil.postCreateUser(name, email, password)
+                .subscribe(new ApiObserver<String>(errorView) {
+                    @Override
+                    public void onNext(String value) {
+                        view.processUserRegisterSuccess();
+                    }
+                });
+    }
+
+    private boolean validateUserInput(String name, String email, String password, String passwordConfirmation) {
+        boolean isValid = true;
+
+        if(StringUtil.isEmpty(name)) {
+            view.showNameValidationError("入力必須です。");
+            isValid = false;
+        }
+        if (StringUtil.isEmpty(email)) {
+            view.showEmailValidationError("入力必須です。");
+            isValid = false;
+        }
+        if (StringUtil.isEmpty(password)) {
             view.showPasswordValidationError("入力必須です。");
+            isValid = false;
         }
-
-        if (!TextUtils.isEmpty(email) && !email.contains("@")) {
+        if (!StringUtil.isEmpty(email) && !email.contains("@")) {
             view.showEmailValidationError("メールアドレスを入力してください。");
+            isValid = false;
         }
-        if (!password.equals(passwordConfirmation)) {
+        if (!StringUtil.isEmpty(password) && !password.equals(passwordConfirmation)) {
             view.showPasswordValidationError("パスワードが合致しません。");
+            isValid = false;
         }
-
-
+        return isValid;
     }
 }
